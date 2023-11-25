c8e49d247a8e6cfca35a6d96b08c59ce3f551416
786213750a <786213750g@gmail.com>
2023-11-25 15:01:00 -0800

    change tests/ to test/ reading pkl

--- test/test_short_results.py
+++ test/test_short_results.py
@@ -1,5 +1,6 @@
 import sys
 import os
+
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.short_results import short_results
 import pandas as pd
@@ -10,19 +11,22 @@ import pickle
 
 @pytest.fixture
 def setup_data():
-      # Assuming the structure of the DataFrame based on the image you provided
-       with open("./tests/model.pkl", "rb") as f:
-              model = pickle.load(f)
-       return model
+    # Assuming the structure of the DataFrame based on the image you provided
+    with open("./test/model.pkl", "rb") as f:
+        model = pickle.load(f)
+    return model
+
 
 def test_input_error(setup_data):
     with pytest.raises(TypeError):
-        short_results([1,2,3])
+        short_results([1, 2, 3])
+
 
 def test_short_results_returns_dataframe(setup_data):
     banana = short_results(setup_data)
     assert isinstance(banana, pd.DataFrame)
 
+
 def test_short_results_correct_size(setup_data):
     banana = short_results(setup_data)
-    assert banana.shape == (5,6)
+    assert banana.shape == (5, 6)
